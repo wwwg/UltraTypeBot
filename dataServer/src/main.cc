b4063b46170c3d42b125e7822f13aa145f45c937
gotzathe <ultratypebot@gmail.com>
2017-06-24 11:12:38 -0500

    Add dataServer listening

--- dataServer/src/main.cc
+++ dataServer/src/main.cc
@@ -1,6 +1,20 @@
-// A test main file to ensure the build system is working properly
 #include "httplib.h"
+#include <iostream>
+#include <string>
+
+#define START_LOCAL_SERVER
+using namespace std;
+
+httplib::Server serv;
 
 int main() {
+	cout << "Starting HTTP server, ";
+	#ifdef START_LOCAL_SERVER
+		cout << "listening locally\n";
+		serv.listen("127.0.0.1", 1234);
+	#else
+		cout << "listening externally\n";
+		serv.listen("204.44.91.137", 1234);
+	#endif
 	return 0;
 }
