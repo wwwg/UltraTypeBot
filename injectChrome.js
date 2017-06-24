ebc5708778c2f0c56464a3472c4a69f461d7db46
gotzathe <ultratypebot@gmail.com>
2017-06-24 13:41:41 -0500

    Lazy debug implementation on content script

--- injectChrome.js
+++ injectChrome.js
@@ -2,8 +2,10 @@
     const USE_LOCAL_DATA_SERVER = false;
     const DATA_ENDPOINT = "http://204.44.91.137:8283/baninfo";
     const DATA_ENDPOINT_LOCAL = "http://127.0.0.1:8283/baninfo";
+    var debug = console.log;
     function sendBanInfo(state) {
         if (typeof state !== "string") {
+            debug("WARN: not sending invalid bot state.");
             return;
         }
         var xhr = new XMLHttpRequest();
