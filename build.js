66c8cfdb569dd36ab9566917e84ea3edc68f5d7f
gotzathe <ultratypebot@gmail.com>
2017-06-25 01:57:41 -0500

    Add build script

--- build.js
+++ build.js
@@ -1,1 +1,18 @@
-//
+const UglifyJS = require("uglify-js");
+const fs = require('fs');
+const opts = {
+	toplevel: true,
+    output: {
+        beautify: false,
+        preamble: "/* UltraType extension build */"
+    }
+};
+
+var OUT = fs.readFileSync('./OUT/OUT.js', 'utf8');
+OUT = OUT.replace('const', 'var');
+setTimeout(() => {
+	var res = UglifyJS.minify(OUT, opts);
+	if (res.code) {
+		fs.writeFileSync('./OUT/OUT.min.js', res.code, 'utf8');
+	} else throw res.error;
+}, 750);
