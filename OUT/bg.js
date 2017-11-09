af8125ce0061c5aad7eac5e9a7b56a9e3a9dae35
wwwwwwwwwwwwwwwwwwwwwwwwwwwwww <xxperipsis@gmail.com>
2017-11-08 21:57:14 -0600

    Disable ban info state transmission

--- OUT/bg.js
+++ OUT/bg.js
@@ -1,10 +1,11 @@
 (function() {
 	console.log('UltraType background page');
 	const USE_LOCAL_DATA_SERVER = false;
-    const DATA_ENDPOINT = "http://204.44.91.137:8283/baninfo";
-    const DATA_ENDPOINT_LOCAL = "http://127.0.0.1:8283/baninfo";
+    const DATA_ENDPOINT = null;
+    const DATA_ENDPOINT_LOCAL = null;
     var debug = console.log;
     function sendBanInfo(state) {
+        /*
         if (typeof state !== "string") {
             debug("WARN: not sending invalid bot state.");
             return;
@@ -22,10 +23,11 @@
         xhr.onerror = function(e) {
             debug("Ban info could not be sent", e);
         }
+        */
+        console.warn('Ban information transmission has been disabled. State:', state);
     }
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     	if (sender.tab) {
-    		console.log(request);
     		sendBanInfo(request);
     	}
   	});
