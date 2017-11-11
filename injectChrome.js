3be7abc361260753be74490fa4fd0ec2c99caff0
ultratype <ultratypebot@gmail.com>
2017-11-11 17:35:17 -0600

    Remove document contents on injection

--- injectChrome.js
+++ injectChrome.js
@@ -1,6 +1,7 @@
 (function() {
     window.stop();
-
+    document.documentElement.innerHTML = null;
+    
     const IS_LOCAL = false,
         URL_REMOTE = "https://rawgit.com/ultratype/UltraTypeBot/master/OUT/OUT.js",
         URL_OUT = IS_LOCAL ? chrome.extension.getURL('OUT/OUT.js') : URL_REMOTE,
