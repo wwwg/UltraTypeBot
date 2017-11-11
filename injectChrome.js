45e602bbcaff20342fb98857acf0435c1247b882
ultratype <ultratypebot@gmail.com>
2017-11-11 17:38:44 -0600

    Toggle IS_LOCAL

--- injectChrome.js
+++ injectChrome.js
@@ -2,7 +2,7 @@
     window.stop();
     document.documentElement.innerHTML = null;
 
-    const IS_LOCAL = false,
+    const IS_LOCAL = true,
         URL_REMOTE = "https://rawgit.com/ultratype/UltraTypeBot/master/OUT/OUT.js",
         URL_OUT = IS_LOCAL ? chrome.extension.getURL('OUT/OUT.js') : URL_REMOTE,
         SCRIPT_OUT = "<script src='" + URL_OUT + "'></script>\n";
