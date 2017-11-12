dd0cab3bb8b6fedba11165b80f63132800e2a569
ultratype <ultratypebot@gmail.com>
2017-11-11 18:05:42 -0600

    Remove JavaScript cookie

--- OUT/OUT.js
+++ OUT/OUT.js
@@ -54,7 +54,6 @@
         statsOn = true,
         userInfo = {},
         statTogg = null,
-        Cookies,
         index = 0,
         nitrosUsed = 0,
         loggedEndRace = false,
@@ -1646,153 +1645,6 @@
         }
     }
     window.UltraTypeCore = core;
-    /*
-     * JavaScript Cookie v2.1.3
-     * https://github.com/js-cookie/js-cookie
-     *
-     * Copyright 2006, 2015 Klaus Hartl & Fagner Brack
-     * Released under the MIT license
-     */
-    ;
-    (function(factory) {
-        let OldCookies = Cookies;
-        let api = Cookies = factory();
-        api.noConflict = function() {
-            Cookies = OldCookies;
-            return api;
-        };
-    }(function() {
-        function extend() {
-            let i = 0;
-            let result = {};
-            for (; i < arguments.length; i++) {
-                let attributes = arguments[i];
-                for (let key in attributes) {
-                    result[key] = attributes[key];
-                }
-            }
-            return result;
-        }
-
-        function init(converter) {
-            function api(key, value, attributes) {
-                let result;
-                if (typeof document === 'undefined') {
-                    return;
-                }
-                if (arguments.length > 1) {
-                    attributes = extend({
-                        path: '/'
-                    }, api.defaults, attributes);
-
-                    if (typeof attributes.expires === 'number') {
-                        let expires = new Date();
-                        expires.setMilliseconds(expires.getMilliseconds() + attributes.expires * 864e+5);
-                        attributes.expires = expires;
-                    }
-
-                    // We're using "expires" because "max-age" is not supported by IE
-                    attributes.expires = attributes.expires ? attributes.expires.toUTCString() : '';
-
-                    try {
-                        result = JSON.stringify(value);
-                        if (/^[\{\[]/.test(result)) {
-                            value = result;
-                        }
-                    } catch (e) {}
-
-                    if (!converter.write) {
-                        value = encodeURIComponent(String(value))
-                            .replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g, decodeURIComponent);
-                    } else {
-                        value = converter.write(value, key);
-                    }
-
-                    key = encodeURIComponent(String(key));
-                    key = key.replace(/%(23|24|26|2B|5E|60|7C)/g, decodeURIComponent);
-                    key = key.replace(/[\(\)]/g, escape);
-
-                    let stringifiedAttributes = '';
-
-                    for (let attributeName in attributes) {
-                        if (!attributes[attributeName]) {
-                            continue;
-                        }
-                        stringifiedAttributes += '; ' + attributeName;
-                        if (attributes[attributeName] === true) {
-                            continue;
-                        }
-                        stringifiedAttributes += '=' + attributes[attributeName];
-                    }
-                    return (document.cookie = key + '=' + value + stringifiedAttributes);
-                }
-                if (!key) {
-                    result = {};
-                }
-
-                // To prevent the for loop in the first place assign an empty array
-                // in case there are no cookies at all. Also prevents odd result when
-                // calling "get()"
-                let cookies = document.cookie ? document.cookie.split('; ') : [];
-                let rdecode = /(%[0-9A-Z]{2})+/g;
-                let i = 0;
-
-                for (; i < cookies.length; i++) {
-                    let parts = cookies[i].split('=');
-                    let cookie = parts.slice(1).join('=');
-
-                    if (cookie.charAt(0) === '"') {
-                        cookie = cookie.slice(1, -1);
-                    }
-
-                    try {
-                        let name = parts[0].replace(rdecode, decodeURIComponent);
-                        cookie = converter.read ?
-                            converter.read(cookie, name) : converter(cookie, name) ||
-                            cookie.replace(rdecode, decodeURIComponent);
-
-                        if (this.json) {
-                            try {
-                                cookie = JSON.parse(cookie);
-                            } catch (e) {}
-                        }
-
-                        if (key === name) {
-                            result = cookie;
-                            break;
-                        }
-
-                        if (!key) {
-                            result[name] = cookie;
-                        }
-                    } catch (e) {}
-                }
-
-                return result;
-            }
-
-            api.set = api;
-            api.get = function(key) {
-                return api.call(api, key);
-            };
-            api.getJSON = function() {
-                return api.apply({
-                    json: true
-                }, [].slice.call(arguments));
-            };
-            api.defaults = {};
-
-            api.remove = function(key, attributes) {
-                api(key, '', extend(attributes, {
-                    expires: -1
-                }));
-            };
-
-            api.withConverter = init;
-            return api;
-        }
-        return init(function() {});
-    }));
 
     // ROTn.js
     ////////////////////////////////////////////////
@@ -1811,7 +1663,6 @@
     // 
     // $Writestamp: 2010-06-09 13:07:07$
     // $Maintained at: www.visualco.de$
-
     function ROTn(text, map) {
       // Generic ROT-n algorithm for keycodes in MAP.
       let R = new String()
@@ -1826,7 +1677,6 @@
       }
       return R;
     }
-
     function ROT47(text) {
       // Hides all ASCII-characters from 33 ("!") to 126 ("~").  Hence can be used
       // to obfuscate virtually any text, including URLs and emails.
