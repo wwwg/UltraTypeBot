6aebfeba0b3ad8867e01b0f806692e376b3d5734
ultratype <ultratypebot@gmail.com>
2017-11-14 12:39:09 -0600

    Improve injection

--- injectChrome.js
+++ injectChrome.js
@@ -1,7 +1,4 @@
 (function() {
-    window.stop();
-    document.documentElement.innerHTML = null;
-
     const IS_LOCAL = true,
         URL_REMOTE = "https://rawgit.com/ultratype/UltraTypeBot/master/OUT/OUT.js",
         URL_OUT = IS_LOCAL ? chrome.extension.getURL('OUT/OUT.js') : URL_REMOTE;
