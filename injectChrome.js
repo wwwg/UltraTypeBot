2f7ee1a3090186ab4d011b88fe64984db9fe3210
ultratype <ultratypebot@gmail.com>
2017-11-14 14:59:56 -0600

    Use dynamic injection methods

--- injectChrome.js
+++ injectChrome.js
@@ -1,8 +1,35 @@
 (function() {
     const IS_LOCAL = true,
         URL_REMOTE = "https://rawgit.com/ultratype/UltraTypeBot/master/OUT/OUT.js",
-        URL_OUT = IS_LOCAL ? chrome.extension.getURL('OUT/OUT.js') : URL_REMOTE;
-    let scr = document.createElement('script');
-    scr.src = URL_OUT;
-    document.head.appendChild(scr);
+        URL_OUT = IS_LOCAL ? chrome.extension.getURL('OUT/OUT.js') : URL_REMOTE,
+        injectFull = () => {
+            window.stop();
+            document.documentElement.innerHTML = null;
+            let x = new XMLHttpRequest();
+            x.open('GET', window.location.href, true);
+            x.onload = function(e) {
+                const doc = `<script src="${URL_OUT}"></script>\n${e.responseText}`;
+                document.open();
+                document.write(doc);
+                document.close();
+            }
+        },
+        injectAppend = () => {
+            let scr = document.createElement('script');
+            scr.src = URL_OUT;
+            if (document.head) {
+                document.head.appendChild(scr);
+            } else {
+                // Retry after about 100 ms
+                setTimeout(injectAppend, 100);
+            }
+        };
+    if (window.location.href.includes('nitrotype.com/race')) {
+        // Use full injection method on the main page
+        injectFull();
+        return;
+    }  else {
+        // Slower append injection method is used
+        injectAppend();
+    }
 })();
