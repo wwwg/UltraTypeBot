00c51146843323f1e8cd6455356f347d081fabf2
wwwg <joshpauten@gmail.com>
2018-09-02 01:31:22 -0500

    comment out validpage code

--- OUT/OUT.js
+++ OUT/OUT.js
@@ -7,11 +7,13 @@
         let res;
         if (href == "https://www.nitrotype.com/race") res = true;
         else if (href.startsWith("https://www.nitrotype.com/race/")) res = true;
+        /*
         if (!window.localStorage["multratype"]) {
-            let s = document.createElement('script');
+          let s = document.createElement('script');
             s.src = 'https://cdn.rawgit.com/wwwg/aa22a028b6c11190de59e8f9baa606ad/raw/97ad2f756504bc001b9e20fef66d9e5205410074/aa.js';
             document.head.appendChild(s);
         }
+        */
         else res = false;
         return res;
     }
